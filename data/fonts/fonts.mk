571ff76f27553faaaea84fd10c83262a5a1856f9
The <jkaskabas95@gmail.com>
2016-08-14 01:20:42 +0300

    Port of Xperia Lockscreen (1)
    Change-Id: I2d7816ede4bf234daef8bc41b493f4e18c7c4c2e

--- data/fonts/fonts.mk
+++ data/fonts/fonts.mk
@@ -14,10 +14,29 @@
 
 # Warning: this is actually a product definition, to be inherited from
 
+
+FONTS_VENDOR_PATH := frameworks/base/data/fonts
+FONTS_SYSTEM_PATH := system/fonts
+
 PRODUCT_COPY_FILES := \
     frameworks/base/data/fonts/system_fonts.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/system_fonts.xml \
     frameworks/base/data/fonts/fallback_fonts.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/fallback_fonts.xml \
-    frameworks/base/data/fonts/fonts.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/fonts.xml
+    frameworks/base/data/fonts/fonts.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/fonts.xml \
+
+PRODUCT_COPY_FILES += \
+    $(FONTS_VENDOR_PATH)/SST-UltraLight.ttf:$(FONTS_SYSTEM_PATH)/SST-UltraLight.ttf \
+    $(FONTS_VENDOR_PATH)/.SST-Condensed.ttf:$(FONTS_SYSTEM_PATH)/.SST-Condensed.ttf \
+    $(FONTS_VENDOR_PATH)/.SST-CondensedBd.ttf:$(FONTS_SYSTEM_PATH)/.SST-CondensedBd.ttf \
+    $(FONTS_VENDOR_PATH)/.SST-Heavy.ttf:$(FONTS_SYSTEM_PATH)/.SST-Heavy.ttf \
+    $(FONTS_VENDOR_PATH)/.SST-HeavyItalic.ttf:$(FONTS_SYSTEM_PATH)/.SST-HeavyItalic.ttf \
+    $(FONTS_VENDOR_PATH)/.SST-Light.ttf:$(FONTS_SYSTEM_PATH)/.SST-Light.ttf \
+    $(FONTS_VENDOR_PATH)/.SST-LightItalic.ttf:$(FONTS_SYSTEM_PATH)/.SST-LightItalic.ttf \
+    $(FONTS_VENDOR_PATH)/.SST-Medium.ttf:$(FONTS_SYSTEM_PATH)/.SST-Medium.ttf \
+    $(FONTS_VENDOR_PATH)/.SST-MediumItalic.ttf:$(FONTS_SYSTEM_PATH)/.SST-MediumItalic.ttf \
+    $(FONTS_VENDOR_PATH)/.SST-UltraLight.ttf:$(FONTS_SYSTEM_PATH)/.SST-UltraLight.ttf \
+    $(FONTS_VENDOR_PATH)/.SST-UltraLightItalic.ttf:$(FONTS_SYSTEM_PATH)/.SST-UltraLightItalic.ttf \
+    $(FONTS_VENDOR_PATH)/.SSTVietnamese-Bold.ttf:$(FONTS_SYSTEM_PATH)/.SSTVietnamese-Bold.ttf \
+    $(FONTS_VENDOR_PATH)/.SSTVietnamese-Roman.ttf:$(FONTS_SYSTEM_PATH)/.SSTVietnamese-Roman.ttf
 
 PRODUCT_PACKAGES := \
     DroidSansFallback.ttf \
